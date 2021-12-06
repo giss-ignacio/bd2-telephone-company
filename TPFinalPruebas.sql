f8d454a6a48ff386db3420d31c32b50576b10944
Nacho Giss <giss.ignacio@gmail.com>
2021-12-05 23:16:32 -0300

    Now there's a look in your eyes, like black holes in the sky

--- TPFinalPruebas.sql
+++ TPFinalPruebas.sql
@@ -7,11 +7,18 @@ EXEC @mensaje=mensajeCumplimientoSLA @cumpleSLA=@tmp
 PRINT @mensaje
 GO
 
--- Transaccion de prueba
+-- trigger update genera  mail
 BEGIN TRANSACTION
   UPDATE TICKET SET COD_ESTADO=3 where NRO_TICKET=2;
-  UPDATE MAIL SET FUE_ENVIADO=1 where NRO_TICKET=2;
+  SELECT * from MAIL where NRO_TICKET=2;
 COMMIT;
+
+-- trigger update con cod estado RESUELTO (4) actualiza fecha de resolucion
+BEGIN TRANSACTION
+  UPDATE TICKET SET COD_ESTADO=4 where NRO_TICKET=2;
+  SELECT * from TICKET where NRO_TICKET=2;
+COMMIT;
+
 
 -- Esperado: El tipo de servicio es inexistente para el nombre Telefoni 
 EXEC CrearServicio N'1234-1235', 1, 1, 28101544, N'DNI', N'Telefoni'
@@ -21,3 +28,5 @@ EXEC CrearServicio N'1234-1235', 1, 1, 28101544, N'LE', N'Telefonia'
 EXEC CrearServicio N'1234-1235', 1, 1, 11111111, N'DNI', N'Telefonia'
 -- Resultado OK
 EXEC CrearServicio N'1234-1235', 1, 1, 28101544, N'DNI', N'Telefonia'
+
+
